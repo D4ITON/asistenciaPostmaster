212559ae8b27455314fbbbdf36bd088ce9c1460e
D4ITON <daitonmh100@gmail.com>
2019-08-13 08:36:54 -0500

    barcodescanner available

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -5,6 +5,7 @@ import { Ionicons } from '@expo/vector-icons';
 
 import HomeScreen from '../screens/HomeScreen';
 import AsistenciaScreen from '../screens/AsistenciaScreen';
+import ScanQRScreen from '../screens/ScanQRScreen';
 
 const tabNavigator = createBottomTabNavigator({
   HomeStack: { screen: HomeScreen,
@@ -18,6 +19,14 @@ const tabNavigator = createBottomTabNavigator({
   AsistenciaStack: { screen: AsistenciaScreen,
     navigationOptions:{
       tabBarLabel: 'Asistencia',
+      tabBarIcon: ({ tintColor }) => (
+        <Ionicons name="md-checkmark-circle" size={32} color="green" />
+      ),
+    }
+  },
+  ScanStack: { screen: ScanQRScreen,
+    navigationOptions:{
+      tabBarLabel: 'Scan QR',
       tabBarIcon: ({ tintColor }) => (
         <Ionicons name="md-checkmark-circle" size={32} color="green" />
       ),
